8a57c7d7d8055618729d45fe4d1fa5f74e6d1fe7
Nina Willis <little.nina98@gmail.com>
2019-06-14 01:53:44 +0900

    Incorporate parentheses

--- calculator.py
+++ calculator.py
@@ -23,14 +23,28 @@ def readMinus(line, index):
   token = {'type': 'MINUS'}
   return token, index + 1
 
+
 def readMult(line, index):
   token = {'type': 'MULTIPLY'}
   return token, index + 1
+
 
 def readDiv(line, index):
   token = {'type': 'DIVIDE'}
   return token, index + 1
 
+
+def readParentheses(line, index):
+  init = index
+  section = [0, 0]
+  if line[index] == '(':
+    token = {'type': 'START'}
+    return token, index + 1
+  else:
+    token = {'type': 'END'}    
+    return token, index + 1
+
+  
 def tokenize(line):
   tokens = []
   index = 0
@@ -45,14 +59,49 @@ def tokenize(line):
       (token, index) = readMult(line, index)
     elif line[index] == '/':
       (token, index) = readDiv(line, index)
+    elif line[index] == '(' or line[index] ==  ')':
+      (token, index) = readParentheses(line, index)
     else:
       print('Invalid character found: ' + line[index])
       exit(1)
     tokens.append(token)
   return tokens
 
+# Determine range of each pair of parentheses
+def group(tokens, index):
+  span = [0, 0]
+  layer = 0
+  span[0] = index
+  index += 1
+  while index < len(tokens):
+    if tokens[index]['type'] == 'END' and layer == 0:
+      break
+    elif tokens[index]['type'] == 'START':
+      layer += 1
+    elif tokens[index]['type'] == 'END':
+      layer -= 1
+    index += 1
+  span[1] = index
+  tokens[span[0]]['span'] = span
+  return span[0]
+
+# Determine order of evaluation
+def organize(tokens):
+  order = []
+  index = 0
+  while index < len(tokens):
+    if tokens[index]['type'] == 'START':
+      if order and tokens[index]['span'][1] < tokens[order[-1]]['span'][1]:
+        temp = order[-1]
+        order[-1] = index
+        order.append(temp)
+      else:
+        order.append(index)
+    index += 1
+  return order
+
 # Evaluate for multiplication and division, return new set of tokens
-def firstEvaluate(tokens):
+def multDiv(tokens):
   newTokens = []
   index = 0
   newIndex = 0
@@ -70,7 +119,7 @@ def firstEvaluate(tokens):
   return newTokens
 
 
-def secondEvaluate(tokens):
+def plusMinus(tokens):
   answer = 0
   tokens.insert(0, {'type': 'PLUS'}) # Insert a dummy '+' token
   index = 1
@@ -84,13 +133,52 @@ def secondEvaluate(tokens):
         print('Invalid syntax')
         exit(1)
     index += 1
-  return answer
+    token = {'type': 'NUMBER', 'number': answer}
+  return token
+
+
+def evaluate(tokens):
+  index = 0
+  while index < len(tokens):
+    if tokens[index]['type'] == 'START':
+      index = group(tokens, index)
+    index += 1
+  order = organize(tokens)
+  # If there are parentheses, evaluate each section in order
+  if order:
+    for i in range(len(order)):
+      section = []
+      count = 0
+      start = order[i]
+      end = start + (tokens[order[i]]['span'][1] - tokens[order[i]]['span'][0])
+      tokens.pop(order[i])
+      count += 1
+      for j in range(start, end):
+        if tokens[start]['type'] == 'END':
+          break
+        else:
+          section.append(tokens[start])
+          tokens.pop(start)
+          count += 1
+      for k in range(i+1, len(order)):
+        if order[k] > order[i]:
+          order[k] -= count
+        else:
+          tokens[order[k]]['span'][1] -= count
+      section = multDiv(section)
+      tokens[start] = plusMinus(section)
+    newTokens = multDiv(tokens)
+    answer = plusMinus(newTokens)['number']
+    return answer
+  else:
+    newTokens = multDiv(tokens)
+    answer = plusMinus(newTokens)['number']
+    return answer
 
 
 def test(line):
   tokens = tokenize(line)
-  newTokens = firstEvaluate(tokens)
-  actualAnswer = secondEvaluate(newTokens)
+  actualAnswer = evaluate(tokens)
   expectedAnswer = eval(line)
   if abs(actualAnswer - expectedAnswer) < 1e-8:
     print("PASS! (%s = %f)" % (line, expectedAnswer))
@@ -115,6 +203,11 @@ def runTest():
   test("1.0*2.0/3")
   test("1.0+2-3.0*4/5.0")
   test("1.2+3.4*5-6/7.8")
+  test("(1+2)")
+  test("(1+2)*(3-4)")
+  test("(1+3*5)/(4-2)")
+  test("(1+3*5)/(4-2*(5+1))")
+  test("(1.0+3.0*5)/(4-2.0*(5.0+1.0))")
   print("==== Test finished! ====\n")
 
 runTest()
@@ -123,6 +216,5 @@ while True:
   print('> ', end="")
   line = input()
   tokens = tokenize(line)
-  newTokens = firstEvaluate(tokens)
-  answer = secondEvaluate(newTokens)
+  answer = evaluate(tokens)
   print("answer = %f\n" % answer)
